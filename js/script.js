6571bb0a42847d942d0abf965a4eb8b903abec28
Nicolas359 <102981506+Nicolas359@users.noreply.github.com>
2023-05-22 22:25:52 -0500

    added decrypt Text function

--- js/script.js
+++ js/script.js
@@ -1,33 +1,61 @@
 var encryptButton = document.querySelector(".boton-encriptar");
-var textInputContainer = document.querySelector(".input-container")
-
-encryptButton.onclick = encryptText;
-
-var vowels = ["a","e","i","o","u"]
-
-var encryptVowels = {
+var decryptButton = document.querySelector(".boton-desencriptar");
+var textInputContainer = document.querySelector(".input-container");
+var vowels = ["a","e","i","o","u"];
+var encryptedVowels = ["ai", "enter", "imes", "ober", "ufat"];
+/* var encryptVowels = {
     a : "ai",
     e : "enter",
     i : "imes",
     o : "ober",
     u : "ufat",
-}
+}; */
+
+encryptButton.onclick = encryptText;
+decryptButton.onclick = decryptText;
 
 function encryptText() {
-    const text = textInputContainer.value;
+    let text = textInputContainer.value;
+    let textArray = text.split("");
     var encryptedText = ""
-    let arr = Array.from(text)
+
+    textArray.forEach(element => {
+        if(vowels.includes(element)){
+            textArray[textArray.indexOf(element)] = encryptedVowels[vowels.indexOf(element)]
+        }
+    });
+
+    encryptedText = textArray.join("")
+
+    console.log(encryptedText)
+}
+
+function decryptText() {
+    let text = textInputContainer.value;
+    var decryptedText = "";
+
+    for(var j = 0; j < encryptedVowels.length; j++){
+        
+        decryptedText = text.replaceAll(encryptedVowels[j],vowels[j]);
+        text = decryptedText;
+    };
+
+    console.log(decryptedText);
+    textInputContainer.value = decryptedText;
+}
+
+/*
+function altEncryptText() {
+    const text = textInputContainer.value;
+    var encryptedText = "";
+    let arr = Array.from(text);
 
     for(let i = 0; i < arr.length; i++){
         if (vowels.includes(arr[i])) {
             encryptedText = encryptedText + encryptVowels[arr[i]];
         }
         else{
-            encryptedText = encryptedText + arr[i]
+            encryptedText = encryptedText + arr[i];
         }
-    };
-
-    
-    console.log(encryptedText)
-    textInputContainer.value = ""
-}
+    }
+} */
